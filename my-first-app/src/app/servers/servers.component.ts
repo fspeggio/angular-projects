52ae0216b08e46bdc82abd5a77ca02ff189aa353
Federica <federica.speggiorin@gmail.com>
2021-07-29 09:07:31 +0200

    Directives

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Test', 'Test 2'];
 
   constructor() {
     setTimeout(() => {
@@ -17,6 +19,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
