d05aaf54ad741001c8f4acc997fa761b0eae3058
Federica Speggiorin <federica.speggiorin@gmail.com>
2021-08-04 12:52:48 +0200

    componentn & data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -21,8 +21,8 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreated = true;
     this.servers.push(this.serverName);
-    this.serverCreationStatus =
-      'Server was created! Name is ' + this.serverName;
+    // this.serverCreationStatus =
+    //   'Server was created! Name is ' + this.serverName;
   }
 
   // onUpdateServerName(event: Event) {
