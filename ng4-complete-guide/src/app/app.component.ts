003b9dfcafd6d6689489286684d4a9137f4af586
Federica Speggiorin <federica.speggiorin@gmail.com>
2021-08-23 17:48:01 +0200

    Iniziata navigazione

--- ng4-complete-guide/src/app/app.component.ts
+++ ng4-complete-guide/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng4-complete-guide';
+  feature = 'recipes';
+
+  onFeatureSelected(feature: string) {
+    this.feature = feature;
+  }
 }
