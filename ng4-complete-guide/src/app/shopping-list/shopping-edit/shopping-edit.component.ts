0c9752fbbb623e5791b670f814b46894e9b16f16
Federica Speggiorin <federica.speggiorin@gmail.com>
2021-08-24 10:28:53 +0200

    Directives

--- ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addIngredient() {
+    console.log(this.nameInputRef);
+    console.log(this.amountInputRef);
+    const ingredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    console.log(ingredient);
+    this.ingredientAdded.emit(ingredient);
   }
-
 }
